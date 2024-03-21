0dee5663e546fed16fb6777e2521e1e1b8083b33
Priyanshu Singh <priyanshutech20@gmail.com>
2024-03-21 19:52:57 +0530

    Fixed FoodPage

--- frontend/src/pages/Food/FoodPage.js
+++ frontend/src/pages/Food/FoodPage.js
@@ -11,16 +11,30 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+
 export default function FoodPage() {
   const [food, setFood] = useState({});
   const { id } = useParams();
-  const { addToCart } = useCart();
   const navigate = useNavigate();
   const { user } = useAuth();
 
-  const handleAddToCart = () => {
+  const { addToCart, cartQuantities, changeQuantity, removeFromCart } = useCart();
+
+  const handleAddToCart = (food) => {
     addToCart(food);
-    navigate('/cart');
+  };
+
+  const handleDecreaseQuantity = (food) => {
+    if (cartQuantities[food.id] > 1) {
+      changeQuantity(food.id, cartQuantities[food.id] - 1);
+    } else {
+      removeFromCart(food.id);
+    }
   };
 
   useEffect(() => {
@@ -33,7 +47,7 @@ export default function FoodPage() {
       ) : (
         <div className={classes.container}>
           <Card sx={{
-            maxWidth: 380,
+            width: 360,
             border: "1px",
             borderColor: "#1d1d1d",
             borderStyle: "solid",
@@ -62,7 +76,25 @@ export default function FoodPage() {
                   <Price className={classes.price} price={food.price} />
                 </Typography>
                 {!user || !user.isAdmin ? (
-                  <button onClick={handleAddToCart}>Add to Cart</button>
+                  cartQuantities[food.id] ? (
+                    <ButtonGroup size='small'>
+                      <Button color="success" sx={{ position: "realtive", fontSize: '12px' }} variant="contained" onClick={() => handleDecreaseQuantity(food)}>
+                        <IconButton size='small' sx={{ padding: "0" }}>
+                          <RemoveIcon sx={{ fontSize: "14px" }} />
+                        </IconButton>
+                      </Button>
+                      <Button color="success" sx={{ position: "realtive", fontSize: '12px' }} variant="contained">
+                        <Typography sx={{ padding: "0", fontSize: "14px" }}>{cartQuantities[food.id]}</Typography>
+                      </Button>
+                      <Button color="success" sx={{ position: "realtive", fontSize: '12px' }} variant="contained" onClick={() => addToCart(food)}>
+                        <IconButton sx={{ padding: "0" }} >
+                          <AddIcon sx={{ fontSize: "14px" }} />
+                        </IconButton>
+                      </Button>
+                    </ButtonGroup>
+                  ) : (
+                    <Button onClick={() => handleAddToCart(food)} color="success" sx={{ position: "realtive", fontSize: '12px' }} variant="contained">Add to Cart</Button>
+                  )
                 ) : user.isAdmin ? <Link to={'/admin/editFood/' + food.id}>Edit</Link>
                   : null}
               </Stack>
